d1b11f0dea7361156b3fef74ab5ce1588d79c5f0
Gideon Ahiadzi <60545561+horlali@users.noreply.github.com>
2023-04-14 01:01:50 +0000

    maps, structs and concurrency

--- helper.go
+++ helper.go
@@ -1,8 +1,8 @@
-package helper
+package main
 
 import "strings"
 
-func ValidateUserInput(firstName string, lastName string, email string, numberOfTickets uint, remainigTickets uint) (bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, numberOfTickets uint, remainigTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@") && strings.Contains(email, ".")
 	isValidNumberOfTickets := numberOfTickets > 0 && numberOfTickets <= remainigTickets
--- main.go
+++ main.go
@@ -2,49 +2,58 @@ package main
 
 import (
 	"fmt"
-	"go-tuts/helper"
-	"strings"
+	"sync"
+	"time"
 )
 
 const conferenceName = "Ticket Booker World"
 const conferenceTickets = 50
 
 var remainigTickets uint = 50
-var bookings = []string{}
+var bookings = make([]UserData, 0)
+
+type UserData = struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	greetUser()
 
-	for {
-		if remainigTickets == 0 {
-			fmt.Println("Sorry we are sold out")
-			break
-		}
-
-		firstName, lastName, email, numberOfTickets := getUserInput()
-		isValidName, isValidEmail, isValidNumberOfTickets := helper.ValidateUserInput(firstName, lastName, email, numberOfTickets, remainigTickets)
-
-		if isValidName && isValidEmail && isValidNumberOfTickets {
-			bookTicket(firstName, lastName, email, numberOfTickets)
-
-			firstNames := getFirstNames()
-			fmt.Printf("Earliest person to book tickets: %v\n", firstNames[0])
-			fmt.Printf("See other people who have booked tickets: %v\n", firstNames)
-
-		} else {
-			if !isValidName {
-				fmt.Println("Name must be at least 2 characters")
-			}
-			if !isValidEmail {
-				fmt.Println("Email must be valid")
-			}
-			if !isValidNumberOfTickets {
-				fmt.Println("Sorry we only have", remainigTickets, "remainig tickets")
-			}
-		}
-
+	if remainigTickets == 0 {
+		fmt.Println("Sorry we are sold out")
+		// break
 	}
 
+	firstName, lastName, email, numberOfTickets := getUserInput()
+	isValidName, isValidEmail, isValidNumberOfTickets := validateUserInput(firstName, lastName, email, numberOfTickets, remainigTickets)
+
+	if isValidName && isValidEmail && isValidNumberOfTickets {
+		bookTicket(firstName, lastName, email, numberOfTickets)
+
+		wg.Add(1)
+		go sendTicket(firstName, lastName, email, numberOfTickets)
+
+		firstNames := getFirstNames()
+		fmt.Printf("Earliest person to book tickets: %v\n", firstNames[0])
+		fmt.Printf("See other people who have booked tickets: %v\n", firstNames)
+
+	} else {
+		if !isValidName {
+			fmt.Println("Name must be at least 2 characters")
+		}
+		if !isValidEmail {
+			fmt.Println("Email must be valid")
+		}
+		if !isValidNumberOfTickets {
+			fmt.Println("Sorry we only have", remainigTickets, "remainig tickets")
+		}
+	}
+	wg.Wait()
 }
 
 func greetUser() {
@@ -55,8 +64,7 @@ func greetUser() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -85,17 +93,34 @@ func getUserInput() (string, string, string, uint) {
 func bookTicket(firstName string, lastName string, email string, numberOfTickets uint) {
 	remainigTickets = remainigTickets - numberOfTickets
 
-	bookings = append(bookings, firstName+" "+lastName)
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: numberOfTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Println(bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. We have sent a confirmation email to %v\n", firstName, lastName, numberOfTickets, email)
 	fmt.Println("We have", remainigTickets, "remainig tickets")
 
 }
 
+func sendTicket(firstName string, lastName string, email string, numberOfTickets uint) {
+	time.Sleep(30 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", numberOfTickets, firstName, lastName)
+	fmt.Println("#############################################")
+	fmt.Printf("Sending ticket:\n %vto email address %v\n", ticket, email)
+	fmt.Println("#############################################")
+	wg.Done()
+}
+
 // A few more things to learn
 // 1. Maps in golang
 // 2. Structs in golang
 // 3. Coroutines and concurrency in golang
-// 4. Interfaces in golang
-// 5. How to write tests in golang
-// 6. How to write benchmarks in golang
+// 4. Interfaces in golang --  not in Nana's tutorial
+// 5. How to write tests in golang --  not in Nana's tutorial
+// 6. How to write benchmarks in golang --  not in Nana's tutorial
